de0d964f7260635568aa25a2c5308385cc30b8b2
Zaid Dagbanbia <mohammed.dagbanbia@dci-student.org>
2024-08-18 20:39:21 +0200

    added frontend link

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -13,7 +13,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 // placing user order for frontend
 
 const placeOrder = async (req,res) => {
-    const frontend_url = "http://localhost:5173"
+    const frontend_url = "https://food-delivery-app-frontend-ecx8.onrender.com"
 
     try {
         const newOrder = new orderModel({
