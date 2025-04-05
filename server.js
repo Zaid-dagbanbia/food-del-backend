33231cde966540dbc62d39ff71d863208d51280c
Zaid <dagbanbia@gmail.com>
2025-04-05 18:50:40 +0200

    updated frontend

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const app = express();
 //middleware
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:5173", 
+    origin: "https://food-del-frontend-tybx.onrender.com", 
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization', 'token'],
     credentials: true
